455dcd5cc75f9171f8e921faa6a157e09a759973
Ivonita <viskha.ivonita@gmail.com>
2023-07-19 11:26:50 +0200

    First commit

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request
+import psycopg2
+
+app = Flask(__name__)
+transactions = []
+POSTGRESQL_URI = "postgres://agcjoewb:vTZicTH5ssYG2fFeS1OcMucJUfIpnLfo@tyke.db.elephantsql.com/agcjoewb"
+
+
+connection = psycopg2.connect(POSTGRESQL_URI)
+
+try:
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(
+                "CREATE TABLE transactions (date TEXT, amount REAL, account TEXT);"
+                )
+except psycopg2.errors.DuplicateTable:
+    pass        
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        print(request.form)
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    "INSERT INTO transactions VALUES (%s, %s, %s);",
+                    (
+                        request.form.get("date"),
+                        float(request.form.get("amount")),
+                        request.form.get("account"),
+                    ),
+                )
+    return render_template("form.html")
+
+@app.route("/transactions")
+def show_transactions():
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM transactions;")
+        transactions = cursor.fetchall()
+    return render_template("transactions.jinja2", entries=transactions)
